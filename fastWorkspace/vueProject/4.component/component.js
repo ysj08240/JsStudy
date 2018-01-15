ae29ad81932b712daa2754382e6233a7c5563994
YuSeungjin <ysj08240@gmail.com>
2018-01-16 00:36:33 +0900

    js study first commit : vue and node js example

---
+++ fastWorkspace/vueProject/4.component/component.js
@@ -0,0 +1,27 @@
+
+Vue.component('button-counter', {
+  template: '<button v-on:click="incrementCounter">{{ counter }}</button>',
+  data: function () {
+    return {
+        counter: 0
+    }
+  },
+  methods: {
+      incrementCounter: function (){
+          this.counter += 1
+          this.$emit('increment')
+      }
+  },
+})
+
+new Vue({
+    el: '#counter-event-example',
+    data: {
+      total: 0
+    },
+    methods: {
+      incrementTotal: function () {
+        this.total += 1
+      }
+    }
+  })
